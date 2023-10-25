fb2895f8987888a8715c3dc987134dfeb501f014
verayuni12 <148970067+verayuni12@users.noreply.github.com>
2023-10-25 18:33:44 +0700

    Create 250x300.js

---
+++ 250x300.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '0c5c83e402fe9b201215a2fcb9ff95d2',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//safeattributeexcept.com/0c5c83e402fe9b201215a2fcb9ff95d2/invoke.js"></scr' + 'ipt>');
+
